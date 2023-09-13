f1bd72e86c40556be377dce08396d9ebbc577a89
daniel1181888 <94381025+daniel1181888@users.noreply.github.com>
2023-09-13 15:45:25 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C_Geoproffs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
